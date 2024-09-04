7103926dd82d7779ed741e881741c487b0e95b6e
AnastasiaS95 <ms.anastasia1artyukh@gmail.com>
2024-09-04 23:48:14 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log('Скрипт 2 был успешно подключен к странице');
+alert('Hello, world');
+console.log('Текущее время:', new Date().toLocaleTimeString());
+alert('Это дополнительное сообщение!');
